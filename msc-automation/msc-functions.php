4f6e3d8c5eb60667d2ec24ac0d5b34e59912db9a
Marti <Marti@192.168.1.106>
2020-10-07 16:16:35 +0200

    version 2.2 beta amb cookies alert

--- msc-automation/msc-functions.php
+++ msc-automation/msc-functions.php
@@ -162,9 +162,7 @@ function bps_cookie_script () {
 <script type="text/javascript">
 window.cookieconsent_options = {"message":"<?php echo $str_message ?>","dismiss":"<?php echo $str_OK ?>","learnMore":"<?php echo $str_moreinfo ?>","link":"<?php echo $str_link ?>","theme":"dark-bottom"};
 </script>
-
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
-
 <?php
 }
 
